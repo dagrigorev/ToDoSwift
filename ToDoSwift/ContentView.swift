01ad03793c24399b46fe4d704ce321d133b6229c
Dmitry Grigorev <jabberwooky@icloud.com>
2023-01-07 23:00:31 +0300

    Initial Commit

---
+++ ToDoSwift/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ToDoSwift
+//
+//  Created by Dmitry Grigorev on 07.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoSwift/ToDoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftApp.swift
+//  ToDoSwift
+//
+//  Created by Dmitry Grigorev on 07.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
